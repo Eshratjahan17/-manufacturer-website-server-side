d22e1a2f55398554dbc35300edd6e55bd6bec379
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-24 15:24:26 +0600

    added orders db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 var jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port =process.env.PORT|| 5000;
 
@@ -23,17 +23,32 @@ async function run(){
   try{
     await client.connect();
     const toolsCollection = client.db("manufacture-car-tools").collection("tools");
-    app.get('/tools',async(req,res)=>{
+    const ordersCollection = client.db("manufacture-car-tools").collection("orders");
+    //All data
+    app.get("/tools", async (req, res) => {
       const q = req.query;
-        console.log(q);
-        const cursor=toolsCollection.find(q);
-        const result=await cursor.toArray();
-        res.send(result);
-
+      console.log(q);
+      const cursor = toolsCollection.find(q);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //specific data
+    //http://localhost:5000/tool/:id
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
+    //orders post
+    app.post('/order',async(req,res)=>{
+      const orders=req.body;
+      const result=await ordersCollection.insertOne(orders);
+      res.send(result);
     })
- 
+
     console.log("db connected");
- }
+  }
   finally{
 
   }
