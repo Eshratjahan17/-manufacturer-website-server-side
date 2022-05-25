15c5cf4bd7f63cbb9726a5d8df7e1756324de4b1
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-25 16:05:43 +0600

    restricted admin options

--- index.js
+++ index.js
@@ -89,6 +89,13 @@ async function run(){
       }
      
     });
+    //check Admin
+    app.get('/admin/:email',async(req,res)=>{
+      const email=req.params.email;
+      const user= await usersCollection.findOne({email:email});
+      const isAdmin=user.role === 'admin';
+      res.send({admin :isAdmin})
+    });
     //get Users
     app.get("/user", async (req, res) => {
       const cursor = usersCollection.find();
