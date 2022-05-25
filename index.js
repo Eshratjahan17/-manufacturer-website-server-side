60c2662e42711be51ba5a48c09cca39b8c71ba7e
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-25 15:00:13 +0600

    admin secure

--- index.js
+++ index.js
@@ -70,18 +70,24 @@ async function run(){
     });
 
     //make Admin
-    app.put("/user/admin/:email", async (req, res) => {
+    app.put("/user/admin/:email",verifyJwt, async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: {role:'admin'},
-      };
+      const requester=req.decoded.email;
+      const requesterAccount=await usersCollection.findOne({email:requester});
+      if(requesterAccount.role === 'admin'){
+         const filter = { email: email };
+         const updateDoc = {
+           $set: { role: "admin" },
+         };
 
-      const result = await usersCollection.updateOne(
-        filter,
-        updateDoc
-      );
-      res.send(result);
+         const result = await usersCollection.updateOne(filter, updateDoc);
+         res.send(result);
+        
+      }
+      else{
+        res.status(403).send({message:'forbidden'});
+      }
+     
     });
     //get Users
     app.get("/user", async (req, res) => {
